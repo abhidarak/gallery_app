d5fe7bb9c2ea7130429a3db45d34d0e32aedce54
abhishekdarak <97934871+abhishekdarak@users.noreply.github.com>
2022-11-23 11:06:31 +0530

    created new iOS project

---
+++ GalleryApp/GalleryApp/GalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppApp.swift
+//  GalleryApp
+//
+//  Created by Abhishek Darak on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
